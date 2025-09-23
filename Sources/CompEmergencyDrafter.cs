c42c2c4fe9bd7f48fa25382b5d3294c26b8105a1
jorodane <jorodane@naver.com>
2025-09-23 17:02:06 +0900

    Add Colonist List In Draft Tab

--- Sources/CompEmergencyDrafter.cs
+++ Sources/CompEmergencyDrafter.cs
@@ -8,20 +8,31 @@ namespace MoreVanillaStructure
 {
     public static class Extension_Pawn
     {
-        public static bool IsDraftable(this Pawn pawn) => pawn.Spawned && pawn.Faction == Faction.OfPlayer && !pawn.DeadOrDowned;
+        public static bool IsDraftable(this Pawn pawn) => pawn.Spawned && pawn.Faction == Faction.OfPlayer && pawn.drafter != null && !pawn.DeadOrDowned && !pawn.InMentalState;
     }
 
     public class ITab_DraftSetting : ITab
     {
-        static readonly Vector2 tabSize = new Vector2(300f,480f);
-        const string tabNameKey = "MVS_Tab_DraftSelecter";
+		static readonly Vector2 tabSize = new Vector2(400f,600f);
+		static readonly Vector2 portraitSize = new Vector2(rowHeight, rowHeight);
+		bool? mouseSelected = null;
 
-        const float headerHeight = 28f;
-        const float rowHeight = 26f;
+        const string tabNameKey = "MVS_Tab_DraftSelecter";
+		const float headerHeight = 28f;
+		const float headerPadding = 12.0f;
+		const float listPadding = 15.0f;
+		const float viewPadding = 16.0f;
+		const float rowPadding = 0.0f;
+        const float rowHeight = 30f;
+		const float checkSize = 24f;
 
         Vector2 scrollPosition;
+		bool allSelected = false;
 
-        public ITab_DraftSetting()
+
+		public string GetDraftAllString() => "MVS_Button_DraftAll".Translate();
+
+		public ITab_DraftSetting()
         {
             size = tabSize;
             labelKey = tabNameKey;
@@ -29,41 +40,109 @@ namespace MoreVanillaStructure
 
         protected override void FillTab()
         {
-            CompEmergencyDrafter drafter = SelThing?.TryGetComp<CompEmergencyDrafter>();
+			Event currentEvent = Event.current;
+
+			CompEmergencyDrafter drafter = SelThing?.TryGetComp<CompEmergencyDrafter>();
             Map currentMap = SelThing?.Map;
             if (drafter == null || currentMap == null) return;
 
             Rect windowRect = new Rect(0f, 0f, size.x, size.y).ContractedBy(10f);
             Rect headerRect = windowRect;
-            headerRect.height = 28f;
+            headerRect.height = headerHeight;
 
             Text.Font = GameFont.Medium;
             Widgets.Label(headerRect, tabNameKey.Translate());
             Text.Font = GameFont.Small;
 
-            List<Pawn> colonists = currentMap.mapPawns.FreeColonistsSpawned;
-            Rect listRect = new Rect(windowRect.x, headerHeight + 6.0f, windowRect.width, windowRect.height - headerHeight - 6.0f);
-            Rect viewRect = new Rect(0f, 0f, listRect.width - 16.0f, colonists.Count * rowHeight);
+			float currentHeight = headerHeight + headerPadding;
 
-            IEnumerable<Pawn> selectedPawn = Find.Selector.SelectedObjectsListForReading.OfType<Pawn>();
+			List<Pawn> colonists = Find.ColonistBar.GetColonistsInOrder();
+			Rect listRect = new Rect(windowRect.x, currentHeight, windowRect.width, windowRect.height - currentHeight - listPadding);
+            Rect viewRect = new Rect(0f, 0f, listRect.width - viewPadding, colonists.Count * rowHeight);
 
-            Widgets.BeginScrollView(listRect, ref scrollPosition, viewRect);
-            Rect rowRect = new Rect(0f, 0f, viewRect.width, rowHeight);
-            Rect checkRect = new Rect(0f,0f,24f,24f);
-            foreach (Pawn currentPawn in colonists)
+            Rect rowRect = new Rect(rowPadding, 0f, viewRect.width, rowHeight);
+            Rect pawnInfoRect = new Rect(rowRect.x, rowRect.y, viewRect.width - checkSize - rowPadding, rowHeight);
+			Rect checkRect = new Rect(viewRect.width - checkSize, rowRect.y, checkSize, checkSize);
+
+			Rect draftAllRect = pawnInfoRect;
+			draftAllRect.x = windowRect.width - 100.0f;
+			draftAllRect.width = 76.0f;
+			draftAllRect.y = currentHeight;
+			listRect.y = currentHeight += draftAllRect.height;
+
+			Rect pawnPortraitRect = pawnInfoRect;
+			pawnPortraitRect.width = rowHeight;
+			Rect pawnDetailRect = pawnPortraitRect;
+			pawnDetailRect.x = pawnPortraitRect.width;
+			Rect pawnNameRect = pawnInfoRect;
+			pawnNameRect.x = pawnDetailRect.x + pawnDetailRect.width;
+			pawnNameRect.width = pawnInfoRect.width - pawnNameRect.x - pawnPortraitRect.width;
+			Rect pawnWeaponRect = pawnPortraitRect;
+			pawnWeaponRect.x = pawnNameRect.x + pawnNameRect.width;
+
+			bool wasAllSelected = allSelected;
+			Widgets.Label(draftAllRect, GetDraftAllString());
+			Widgets.Checkbox(draftAllRect.position + (Vector2.right * draftAllRect.width), ref allSelected);
+			if (wasAllSelected != allSelected) drafter.SetSelected(colonists, allSelected);
+
+			bool isLeftClick = currentEvent.button == 0;
+			if (!isLeftClick && mouseSelected.HasValue) mouseSelected = null;
+			
+			Widgets.BeginScrollView(listRect, ref scrollPosition, viewRect);
+			allSelected = true;
+			foreach (Pawn currentPawn in colonists)
             {
+				RenderTexture currentPortrait = PortraitsCache.Get(currentPawn, portraitSize, Rot4.South);
+				ThingWithComps weapon = currentPawn.equipment?.Primary;
                 Widgets.DrawHighlightIfMouseover(rowRect);
-                if(selectedPawn.Contains(currentPawn)) Widgets.DrawLightHighlight(rowRect);
+                if(currentPawn.Drafted) Widgets.DrawLightHighlight(rowRect);
 
-                bool isSelected = drafter.IsSelected(currentPawn);
-                checkRect.x = rowRect.x;
-                checkRect.y = rowRect.y;
+				bool wasSelected = drafter.IsSelected(currentPawn);
+				bool isSelected = wasSelected;
+				bool tempSelected = isSelected;
+				checkRect.y = rowRect.y;
+				GUI.DrawTexture(pawnPortraitRect, currentPortrait);
+				TooltipHandler.TipRegion(pawnPortraitRect, new TipSignal($"{currentPawn.LabelCap}\n{currentPawn.GetInspectString()}", currentPawn.thingIDNumber));
+				Widgets.InfoCardButton(pawnDetailRect.x, pawnDetailRect.y, currentPawn);
+				if(weapon != null)
+				{
+					Widgets.ThingIcon(pawnWeaponRect, weapon);
+					TooltipHandler.TipRegion(pawnWeaponRect, new TipSignal(weapon.LabelCapNoCount, weapon.thingIDNumber));
+				}
+				Widgets.Label(pawnNameRect, currentPawn.LabelCap);
+                Widgets.Checkbox(checkRect.position, ref tempSelected);
+				if(currentEvent.isMouse && checkRect.Contains(currentEvent.mousePosition) && isLeftClick)
+				{
+					if(currentEvent.type == EventType.MouseDown) mouseSelected = isSelected = !wasSelected;
+					else if(mouseSelected.HasValue)
+					{
+						if (currentEvent.type == EventType.MouseDrag)	isSelected = mouseSelected.Value;
+						else											mouseSelected = null;
+					}
+				}
 
-                Widgets.Checkbox(checkRect.position,ref isSelected);
+				if (isSelected != wasSelected)
+				{
+					drafter.SetSelected(currentPawn, isSelected);
+				}
 
-                rowRect.y += rowHeight;
+				if(Widgets.ButtonInvisible(pawnInfoRect))
+				{
+					switch (currentEvent.button)
+					{
+						case 0:
+							CameraJumper.TryJump(currentPawn);
+							break;
+					}
+					currentEvent.Use();
+				}
+
+				pawnPortraitRect.y = pawnDetailRect.y = pawnWeaponRect.y = pawnNameRect.y = pawnInfoRect.y = rowRect.y += rowHeight;
+				allSelected &= isSelected;
             }
             Widgets.EndScrollView();
+
+			currentHeight += colonists.Count * rowHeight;
         }
     }
 
@@ -95,12 +174,29 @@ namespace MoreVanillaStructure
         public bool HasDraftableSelectedPawn => selectedPawn.Any((current) => current != null && current.IsDraftable());
 
         public bool IsSelected(Pawn pawn) => selectedPawn.Contains(pawn);
+		public void ToggleSelected(Pawn pawn) { if (IsSelected(pawn)) selectedPawn.Remove(pawn); else selectedPawn.Add(pawn); }
+		public void SetSelected(Pawn pawn, bool value) { if (value) { if(!IsSelected(pawn))selectedPawn.Add(pawn); } else selectedPawn.Remove(pawn); }
+		public void SetSelected(List<Pawn> newList, bool value) 
+		{
+			selectedPawn.Clear();
+			if (value) selectedPawn.AddRange(newList); 
+		}
+
+		public override void PostExposeData()
+		{
+			base.PostExposeData();
+			Scribe_Collections.Look(ref selectedPawn, "SelectedPawn", LookMode.Reference);
+			if(Scribe.mode == LoadSaveMode.PostLoadInit)
+			{
+				selectedPawn?.RemoveAll((currentPawn) => currentPawn.DestroyedOrNull());
+			}
+		}
 
         public override IEnumerable<Gizmo> CompGetGizmosExtra()
         {
             yield return new Command_Action
             {
-                Disabled = !HasSelectedPawn,
+                Disabled = !HasDraftableSelectedPawn,
                 disabledReason = !HasSelectedPawn ? GetCallToArms4NotSelectedDescriptionString() : !HasDraftableSelectedPawn ? GetCallToArms4HasNotDraftableDescriptionString() : "",
                 defaultLabel = GetCallToArms4SelectedLableString(),
                 defaultDesc = GetCallToArms4SelectedDescriptionString(),
@@ -119,15 +215,25 @@ namespace MoreVanillaStructure
 
         public void OnCallToArms4Selected()
         {
-            if (!HasSelectedPawn)
-            {
-
-            }
+            foreach(Pawn currentPawn in selectedPawn)
+			{
+				if(currentPawn.IsDraftable())
+				{
+					currentPawn.drafter.Drafted = true;
+				}
+			}
         }
 
         public void OnCallToArms4All()
         {
-
-        }
-    }
+			List<Pawn> colonists = Find.ColonistBar.GetColonistsInOrder();
+			foreach (Pawn currentPawn in colonists)
+			{
+				if (currentPawn.IsDraftable() && currentPawn.Map == parent.Map)
+				{
+					currentPawn.drafter.Drafted = true;
+				}
+			}
+		}
+	}
 }
